0478d28a858492b199929f72954845a75f73e225
Nick-Hericks <nickhericks@gmail.com>
2020-04-04 15:25:40 -0500

    pull user video

--- pixelated-face-2.js
+++ pixelated-face-2.js
@@ -1,7 +1,26 @@
-console.log('it works');
 const video = document.querySelector('.webcam');
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
 
 const faceCanvas = document.querySelector('.face');
+const faceCtx = canvas.getContext('2d');
 
+const faceDetector = new window.FaceDetector();
+
+console.log(video, canvas, faceCanvas, faceDetector);
+
+//write a function that will populate the user's video
+async function populateVideo() {
+	// grab feed from user's web cam
+	const stream = await navigator.mediaDevices.getUserMedia({
+		video: { width: 1280, height: 720 }
+	})
+
+	video.srcObject = stream;
+	await video.play();
+
+	// size the canvases to be the same size as the video
+	
+}
+
+populateVideo();
