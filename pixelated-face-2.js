ba4d5a03917d2b3b91ec6649647a2b0b49f2e023
Nick-Hericks <nickhericks@gmail.com>
2020-04-05 12:21:22 -0500

    fix canvas height width error

--- pixelated-face-2.js
+++ pixelated-face-2.js
@@ -27,16 +27,31 @@ async function populateVideo() {
 
 	// size the canvases to be the same size as the video
 	// this will allow us to line everything up when applying the face blur
-	canvas.width = video.videowidth;
-	canvas.height = video.videheight;
-	faceCanvas.width = video.videowidth;
-	faceCanvas.height = video.videheight;
+	canvas.width = video.videoWidth;
+	canvas.height = video.videoHeight;
+	faceCanvas.width = video.videoWidth;
+	faceCanvas.height = video.videoHeight;
 }
 
 async function detect() {
 	const faces = await faceDetector.detect(video);
-	console.log(faces);
+	// console.log(faces);
 
+	// in order to detect the face over and over again, we want to repeatedly call detect. We could use setInterval for this but that will run it on a set time, instead of when the computer running it is actually ready. 
+	// requestAnimationFrame allows us to have the browser tell us when it is ready to update the frame, then do something when it is ready.
+	// ask browser when the next animation frame is and tell it to run detect for us
+	faces.forEach(drawFace);
+	requestAnimationFrame(detect);
+}
+
+function drawFace(face) {
+	// console.log(face);
+	const { width, height, top, left } = face.boundingBox;
+	console.log( {width, height, top, left} );
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.strokeStyle = "#ffc600";
+  ctx.lineWidth = 2;
+	ctx.strokeRect(left, top, width, height);
 }
 
 populateVideo().then(detect);
